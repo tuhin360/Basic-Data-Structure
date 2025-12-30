f494b48a58718d8e4c8ba39fb1ed42fdb1ecc349
tuhin <tuhin153695@gmail.com>
2025-12-30 11:54:28 +0600

    time and space complexity and stl vector

---
+++ Module 02 - STL Vector/vector_modifier.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 2, 6, 100};
+    // vector<int> v2={100, 200};
+   
+    // v.insert(v.begin()+2, v2.begin(), v2.end());
+
+    // v.erase(v.begin()+1, v.begin()+5);
+
+    // replace(v.begin(), v.end()-2, 2, 500);
+
+    // for(int x: v)
+    // { 
+    //     cout << x << " ";
+    // }
+
+    // auto it = find(v.begin(), v.end(), 100);
+
+    // if(it==v.end())
+    // {
+    //     cout << "Not Found";
+    // }
+    // else
+    // {
+    //     cout << "Found";
+    // }
+
+
+    // cout << v[3] << endl;
+
+    // cout << v[v.size()-1]<< endl;
+    // cout << v.back();
+    // cout << v.front() << endl;; 
+
+    for(auto it = v.begin(); it<v.end();it++)
+    {
+
+    }
+
+    return 0;
+}
