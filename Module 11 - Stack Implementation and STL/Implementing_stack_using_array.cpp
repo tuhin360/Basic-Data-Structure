904a1f630ea4014757daf94f2c4268aee0526159
tuhin <tuhin153695@gmail.com>
2026-04-26 21:54:34 +0600

    stack implementation using array

---
+++ Module 11 - Stack Implementation and STL/Implementing_stack_using_array.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myStack
+{
+public:
+    vector<int> v;
+
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+
+    void pop()
+    {
+        v.pop_back();
+    }
+
+    int top()
+    {
+        return v.back();
+    }
+
+    int size()
+    {
+        return v.size();
+    }
+
+    bool empty()
+    {
+        return v.empty();
+    }
+};
+
+int main()
+{
+    myStack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    if (!st.empty())
+        cout << st.top() << endl;
+    if (!st.empty())
+        st.pop();
+
+    return 0;
+}
